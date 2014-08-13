387d9e39c21c3f831ba61a6d16b9d65e5c2c1de3
Nishnilanka <nishan.nilanka@gmail.com>
2014-08-13 15:49:41 +0530

    Initial commit

---
+++ src/fundadministrationsystem/FundAdministrationSystem.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fundadministrationsystem;
+
+/**
+ *
+ * @author Group 7
+ */
+public class FundAdministrationSystem {
+
+    public static void main(String[] args) {
+        
+        Login login = new Login();
+        login.setVisible(true);
+    }
+}
