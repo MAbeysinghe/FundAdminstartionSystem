9a21f4534d304166ec7fba2245d529c5c249bc11
Nishnilanka <nishan.nilanka@gmail.com>
2014-08-14 17:53:45 +0530

    Add login image

---
+++ src/User/ChiefOfficer.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package User;
+
+/**
+ *
+ * @author Nishan Gunawardena
+ */
+public class ChiefOfficer extends User{
+    
+}
